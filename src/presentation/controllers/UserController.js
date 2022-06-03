85c81a1062b015086509fc72364fa4650b860532
Juliana <julianakolmar@gmail.com>
2022-06-03 16:33:49 -0300

    up

--- src/presentation/controllers/UserController.js
+++ src/presentation/controllers/UserController.js
@@ -1,17 +1,7 @@
 class UserController {
 
     handle(req, res, next) {
-        const nome = req.body.nome;
-        const email = req.body.email;
-        const confemail = req.body.confemail;
-        const cpf = req.body.cpf;
-        const telefone = req.body.telefone;
-        const nascimento = req.body.nascimento;
-
-        email_sms (boolean)
-        
-        whatsapp (boolean)
-        res.send("Hello World!");
+        res.send({idade:req.body.idade, name:req.body.name});
     }
 }
 
